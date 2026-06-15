94e70bd6714aadfb4bb40ad62d3e93ded5ae6602
motar <motaruc@gmail.com>
2026-06-15 14:45:52 +0800

    Disable PGlite for Render

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,9 @@
 import * as schema from './schema.ts';
 
-// Temporary Render deployment test.
-// This disables PGlite completely so we can determine
-// whether the Render crash is being caused by database startup.
-
 export const client = {} as any;
 export const db = {} as any;
 
 export async function initDb() {
-  console.log('SKIPPING DB INIT FOR RENDER TEST');
+  console.log('SKIPPING DB INIT FOR RENDER');
   return;
 }
